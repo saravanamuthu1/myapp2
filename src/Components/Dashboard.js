07255c8d5697c09ba5bc1706b875b47e1b61ed5b
saravanamuthu <saravanamuthu.97@gmail.com>
2022-06-14 21:34:20 -0500

    add dashboard page

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Card,Container,Button } from 'react-bootstrap';
 import ReactDOM from 'react-dom/client';
-
+import InputGroup from 'react-bootstrap/InputGroup';
+import { Form } from 'react-bootstrap';
+import { useState } from 'react';
 function Dashboard(){
+    const [publication, addpublication] = useState([])
     return(
-        <h1>this is a Dashboard</h1>
+      <Container className='align-items-center justify-content-center'>
+        <h1>Publications</h1>
+        <Form>
+        <Form.Group id="input"></Form.Group>
+        <InputGroup>
+        <InputGroup.Text>Enter the publication details</InputGroup.Text>
+        <Form.Control as="textarea" aria-label="With textarea" />
+        </InputGroup>
+        <Button variant="primary" type="submit">
+            Add publication 
+        </Button>
+        </Form>
+    </Container>
     )
 }
 export default Dashboard
