b5bd7a182b5eda6cfc21422aa072bcefd4dc20ff
saravanamuthu <saravanamuthu.97@gmail.com>
2022-06-19 12:09:06 -0500

    Updates

---
+++ precache-manifest.931e648f42d4f74564e188ca8adc5e21.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "dda21102c8d713b4808a",
+    "url": "/myapp2/static/css/main.59e9a83a.chunk.css"
+  },
+  {
+    "revision": "dda21102c8d713b4808a",
+    "url": "/myapp2/static/js/main.dda21102.chunk.js"
+  },
+  {
+    "revision": "05b503beb4d93191790c",
+    "url": "/myapp2/static/js/1.05b503be.chunk.js"
+  },
+  {
+    "revision": "539e1152b6ab62cb5a79",
+    "url": "/myapp2/static/css/2.22357f49.chunk.css"
+  },
+  {
+    "revision": "539e1152b6ab62cb5a79",
+    "url": "/myapp2/static/js/2.539e1152.chunk.js"
+  },
+  {
+    "revision": "c44b11d5f6029cf8d8c0",
+    "url": "/myapp2/static/js/runtime~main.c44b11d5.js"
+  },
+  {
+    "revision": "6e8a756097fe93e2497d388afb665f92",
+    "url": "/myapp2/index.html"
+  }
+];
